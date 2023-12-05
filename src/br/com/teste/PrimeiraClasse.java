405f294a73148215cfbd542e734eee4124e9b8af
Felipe Batista <feljava32@gmail.com>
2023-12-05 14:07:54 -0300

    primeiro commit

---
+++ src/br/com/teste/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package br.com.teste;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String teste = args[0];
+		
+	     if(teste.equals("DEV")) {}
+	     System.out.println(teste);	
+	}
+
+}
